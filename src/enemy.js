14e39e34c93fbb8e4fe8cf94734eb48dae73e8ec
Rafael Bosch <rbllado@outlook.com>
2019-11-05 14:00:28 +0100

    update style

--- src/enemy.js
+++ src/enemy.js
@@ -3,7 +3,7 @@
 function Enemy(canvas, x, speed) {
   this.canvas = canvas;
   this.ctx = canvas.getContext('2d');
-  this.size = 20;
+  this.size = 30;
   this.x = x;
   // Creo que no esta bien, le he puesto a groso modo.
   this.y = 20;
@@ -11,9 +11,15 @@ function Enemy(canvas, x, speed) {
 }
 
 Enemy.prototype.draw = function() {
-  this.ctx.fillStyle = '#FF6F27';
+  // this.ctx.fillStyle = '#FF6F27';
   // fillRect(x, y, width, height)
-  this.ctx.fillRect(this.x, this.y, this.size, this.size);
+  // this.ctx.fillRect(this.x, this.y, this.size, this.size);
+
+  let img = new Image(); // Create new <img> element
+  img.src = "../images/nave2Starwars.jpg"; // Set source path
+  
+  // Render the image on the canvas
+  this.ctx.drawImage(img, this.x, this.y, this.size, this.size);
 };
 
 Enemy.prototype.updatePosition = function() {
